0fbfd4458ad6e88117264c59d4922222f86739d2
Rui Gonçalves <ruippeixotog@gmail.com>
2013-12-15 00:16:49 +0000

    Round 2 submission commit.

--- README.md
+++ README.md
@@ -32,12 +32,16 @@ The following is the list of the problems solved. Each problem id is specified b
 * ✓ [35: Basketball Game][qual2] (`basketball-game`)
 * ✓ [45: Tennison][qual3] (`tennison`) [only the Scala version has the required precision]
 
-### Qualification Round
+### Round 1
 
 * ✓ [15: Labelmaker][round11] (`labelmaker`)
 * ✗ [20: Coins Game][round12] (`coins-game`)
 * ✓ [25: AAAAAA][round13] (`aaaaaa`)
 * ✗ [40: Preventing Alzheimer's][round14] (`preventing-alzheimers`)
+
+### Round 2
+
+* ✗ [25: Magic Pairs][round21] (`magic-pairs`)
 
 [1]: https://www.facebook.com/hackercup
 [2]: http://www.scala-lang.org
@@ -49,3 +53,4 @@ The following is the list of the problems solved. Each problem id is specified b
 [round12]: https://www.facebook.com/hackercup/problems.php?pid=105395349584705&round=1437956993099239
 [round13]: https://www.facebook.com/hackercup/problems.php?pid=184326661771870&round=1437956993099239
 [round14]: https://www.facebook.com/hackercup/problems.php?pid=1420024724897316&round=1437956993099239
+[round21]: https://www.facebook.com/hackercup/problems.php?pid=620734011349888&round=544142832342014
---
+++ round2/magic-pairs.cpp
@@ -0,0 +1,99 @@
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <set>
+#include <vector>
+
+#define ll long long
+
+#define MAXN 1000000
+#define MAXM 1000000
+
+using namespace std;
+
+int n, m;
+int x1, a1, b1, c1, r1;
+int x2, a2, b2, c2, r2;
+
+int board1[MAXN], board2[MAXM];
+
+void gen() {
+  set<int> seen1, seen2;
+  vector<int> rep1, rep2;
+
+  board1[0] = x1; seen1.insert(x1);
+  board2[0] = x2; seen2.insert(x2);
+
+  for(int i = 1; i < max(n, m); i++) {
+    if(i < n) {
+      board1[i] = (a1 * board1[(i - 1) % n] + b1 * board2[(i - 1) % m] + c1) % r1;
+      if(seen1.count(board1[i])) rep1.push_back(i); 
+      else { seen1.insert(board1[i]); }
+    }
+    if(i < m) {
+      board2[i] = (a2 * board1[(i - 1) % n] + b2 * board2[(i - 1) % m] + c2) % r2;
+      if(seen2.count(board2[i])) rep2.push_back(i);
+      else { seen2.insert(board2[i]); }
+    }
+  }
+
+  for(int i = 0; i < (int) rep1.size(); i++) board1[rep1[i]] = -1;
+  for(int i = 0; i < (int) rep2.size(); i++) board2[rep2[i]] = -1;
+}
+
+int main() {
+  int t; cin >> t;
+  for(int tc = 1; tc <= t; tc++) {
+    cin >> n >> m;
+    cin >> x1 >> a1 >> b1 >> c1 >> r1;
+    cin >> x2 >> a2 >> b2 >> c2 >> r2;
+
+    gen();
+
+    // for(int i = 0; i < n; i++) {
+    //   cerr << board1[i] << " "; 
+    // }
+    // cerr << endl;
+
+    // for(int i = 0; i < m; i++) {
+    //   cerr << board2[i] << " "; 
+    // }
+    // cerr << endl;
+
+    ll count = 0;
+    int seenSize1 = 0;
+    set<int> seen2;
+
+    for(int i1 = 0, i2 = 0; i1 < n; i1++) {
+      if(board1[i1] == -1) continue;
+
+      // cerr << "i1 = " << i1 << ", i2 = " << i2 << endl;
+      int pairs = 0;
+
+      seenSize1++;
+      while(i2 < m && !seen2.count(board1[i1])) {
+        int color = board2[i2++];
+        if(color != -1) seen2.insert(color);
+      }
+
+      if(i2 == m && !seen2.count(board1[i1])) break;
+
+      if(seenSize1 == (int) seen2.size()) {
+        pairs++;
+        while(i2 < m && board2[i2] == -1) { pairs++; i2++; }
+        // cerr << "pairs are " << pairs << endl;
+
+        count += pairs;
+        i1++;
+        while(i1 < n && board1[i1] == -1) {
+          count += pairs; i1++;
+          // cerr << "pairs are " << pairs << endl;
+        }
+        i1--;
+      }
+    }
+
+    cout << "Case #" << tc << ": " << count << endl;
+  }
+  return 0;
+}
